9a4e687b0ca089d34d0d6dcbffd94a0cbd01f5d9
Luke Hampton <lukehampton6@gmail.com>
2021-09-07 16:13:35 -0400

    tech section started

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './components/Nav';
 import Hero from './components/Hero';
+import Technologies from './components/Technologies';
 
 function App() {
   return (
     <div>
       <Nav />
       <Hero />
+      <Technologies />
     </div>
   );
 }
--- src/components/Technologies/index.js
+++ src/components/Technologies/index.js
@@ -1,5 +1,61 @@
-function Technologies() {
+import {Container, Col, Row} from "react-bootstrap";
+import "devicon/devicon.min.css";
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
+function Technologies() {
+    useEffect(() => {
+        Aos.init({duration: 1500});
+    }, []);
+
+    return (
+        <div className="light">
+        <Container>
+            <h2 data-aos="fade-down" className="dark-text header-two center">Technologies</h2>
+            <Row className="icon-container">
+                <Col data-aos="fade-right" md="auto">
+                <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt=""/>
+                </Col>
+                <Col data-aos="fade-left" md="auto">
+                <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt=""/>
+                </Col>
+            </Row>
+            <Row className="icon-container">
+                <Col data-aos="fade-right" md="auto">
+                <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt=""/>
+                </Col>
+                <Col data-aos="fade-left" md="auto">
+                <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt=""/>
+                </Col>
+            </Row>
+            <Row className="icon-container">
+                <Col data-aos="fade-right" md="auto">
+                <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt=""/>
+                </Col>
+                <Col data-aos="fade-left" md="auto">
+                <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt=""/>
+                </Col>
+            </Row>
+            <Row className="icon-container">
+                <Col data-aos="fade-right" md="auto">
+                <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt=""/>
+                </Col>
+                <Col data-aos="fade-left" md="auto">
+                <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" alt=""/>
+                </Col>
+            </Row>
+            <Row className="icon-container">
+                <Col data-aos="fade-right" md="auto">
+                <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg" alt=""/>
+                </Col>
+                <Col data-aos="fade-left" md="auto">
+                <img className="icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt=""/>
+                </Col>
+            </Row>
+        </Container>
+        </div>
+    )
 }
 
 export default Technologies;
