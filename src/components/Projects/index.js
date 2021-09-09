7f95d7ac94acdb999b114a5364e356833155e070
Luke Hampton <lukehampton6@gmail.com>
2021-09-09 09:34:55 -0400

    card spacing

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,22 +1,17 @@
-import {
-    Row,
-    Col,
-    Card,
-    Container
-} from "react-bootstrap";
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import { Row, Col, Card, Container, CardGroup } from "react-bootstrap";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import { useEffect } from "react";
 
 function Projects() {
-    useEffect(() => {
-        Aos.init({duration: 1500});
-    }, []);
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
 
   return (
     <div className="light">
-    <Container className="card-div">
-    <h2 data-aos="fade-down" className="dark-text header-two center">Projects</h2>
+      <Container className="card-div">
+        {/* <h2 data-aos="fade-down" className="dark-text header-two center">Projects</h2>
       <Row xs={1} md={2} className="g-4">
         {Array.from({ length: 4 }).map((_, idx) => (
           <Col>
@@ -33,8 +28,52 @@ function Projects() {
             </Card>
           </Col>
         ))}
-      </Row>
-    </Container>
+      </Row> */}
+      <h2 data-aos="fade-down" className="dark-text header-two center">Projects</h2>
+        <CardGroup>
+          <Card data-aos="flip-left" className="card-container">
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card data-aos="flip-left" className="card-container">
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{" "}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card data-aos="flip-left" className="card-container">
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardGroup>
+      </Container>
     </div>
   );
 }
